91ab45c928123fe107e3563ee003bb255a90fd43
Gun.io Whitespace Robot <contact@gun.io>
2011-12-18 19:45:34 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- lib/merb_couchrest/merbtasks.rb
+++ lib/merb_couchrest/merbtasks.rb
@@ -1,23 +1,23 @@
 namespace :couchrest do
 
-  
+
   task :merb_start do
     Merb.start :adapter => 'runner',
                :environment => ENV['MERB_ENV'] || 'development'
   end
-  
+
   namespace :sessions do
-    
+
     desc "Clears sessions"
     task :clear => :merb_start do
-      
+
       Merb::CouchRestSessionStore.all.each{ |doc| doc.database.delete_doc(doc,true) }
       Merb::CouchRestSessionStore.database.bulk_save()
-      
+
     end
-    
+
   end
-  
-  
-  
+
+
+
 end
